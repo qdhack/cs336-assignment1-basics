0074b09a5f6342d3071b91290510f0c83b1f2ca1
DDC <14854852+qdhack@users.noreply.github.com>
2026-01-26 21:36:18 +0000

    RoPE

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -115,3 +115,37 @@ class SwiGLU(torch.nn.Module):
 #         silu = silu_activation(a1)
 #         return self.w2(silu)
     
+
+class RotaryPositionalEmbedding(torch.nn.Module):
+    def __init__(
+        self,
+        theta: float,
+        d_k: int,
+        max_seq_len: int,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
+        super().__init__()
+
+        positions = torch.arange(max_seq_len, device=device).unsqueeze(1)
+        freqs = torch.arange(0, d_k, 2, device=device) / d_k
+        inv_freq = 1.0 / (theta**freqs)
+        angles = positions * inv_freq
+
+        self.register_buffer("cos", angles.cos().to(dtype), persistent=False)
+        self.register_buffer("sin", angles.sin().to(dtype), persistent=False)
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        cos_pos = self.cos[token_positions]
+        sin_pos = self.sin[token_positions]
+
+        x_even = x[..., 0::2]
+        x_odd = x[..., 1::2]
+
+        x_rot_even = x_even * cos_pos - x_odd * sin_pos
+        x_rot_odd = x_even * sin_pos + x_odd * cos_pos
+
+        x_rot = rearrange([x_rot_even, x_rot_odd], "two ... -> ... two")
+        x_out = rearrange(x_rot, "... d1 d2 -> ... (d1 d2)")
+
+        return x_out
--- tests/adapters.py
+++ tests/adapters.py
@@ -210,7 +210,8 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    rope = cs336_basics.model.RotaryPositionalEmbedding(theta, d_k, max_seq_len)
+    return rope.forward(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
