d2339081da7dd7ba85554f2dd90354d28e1d54b9
DDC <14854852+qdhack@users.noreply.github.com>
2026-01-26 16:49:06 +0000

    Root Mean Square Layer Normalization

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -66,3 +66,20 @@ class Embedding(torch.nn.Module):
 
         # PyTorch's "Advanced Indexing" https://gemini.google.com/app/c0055d454423ef32
         return self.weight[token_ids]
+    
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+        super().__init__()
+
+        self.eps = eps
+        self.weight = torch.nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+
+        rms = torch.sqrt(reduce(x**2, "... d -> ... 1", "mean") + self.eps)
+        result = x * self.weight / rms
+
+        return result.to(in_dtype)
--- tests/adapters.py
+++ tests/adapters.py
@@ -32,7 +32,7 @@ def run_linear(
     """
 
     layer = cs336_basics.model.Linear(d_in, d_out)
-    layer.weight.data = weights.clone()
+    layer.weight.data.copy_(weights)
     return layer.forward(in_features)
 
 
@@ -56,7 +56,7 @@ def run_embedding(
     """
 
     layer = cs336_basics.model.Embedding(vocab_size, d_model)
-    layer.weight.data = weights.clone()
+    layer.weight.data.copy_(weights)
     return layer.forward(token_ids)
 
 
@@ -384,7 +384,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    layer = cs336_basics.model.RMSNorm(d_model, eps)
+    layer.weight.data.copy_(weights)
+    return layer.forward(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
