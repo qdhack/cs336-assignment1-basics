8887047d9354c096483063510c7479feea9848fc
DDC <14854852+qdhack@users.noreply.github.com>
2026-01-26 19:36:29 +0000

    positionwise_feedforward

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -83,3 +83,35 @@ class RMSNorm(torch.nn.Module):
         result = x * self.weight / rms
 
         return result.to(in_dtype)
+    
+
+def silu_activation(x: torch.Tensor) -> torch.Tensor:
+    return x * torch.sigmoid(x)  # element-wise
+
+
+class SwiGLU(torch.nn.Module):
+    def __init__(self, d_model: int, d_ff: int, device: torch.device | None = None, dtype: torch.dtype | None = None):
+        super().__init__()
+
+        self.w1 = Linear(d_model, d_ff, device, dtype)
+        self.w2 = Linear(d_ff, d_model, device, dtype)
+        self.w3 = Linear(d_model, d_ff, device, dtype)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        a1 = self.w1(x)
+        silu = silu_activation(a1)
+        return self.w2(silu * self.w3(x))
+
+
+# class SiLU(torch.nn.Module):
+#     def __init__(self, d_model: int, d_ff: int, device: torch.device | None = None, dtype: torch.dtype | None = None):
+#         super().__init__()
+
+#         self.w1 = Linear(d_model, d_ff, device, dtype)
+#         self.w2 = Linear(d_ff, d_model, device, dtype)
+
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         a1 = self.w1(x)
+#         silu = silu_activation(a1)
+#         return self.w2(silu)
+    
